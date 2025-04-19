72ddafbf8a99f0f15ef4193335a7905af542c15c
Hung Hao Han <lequanghung.work@gmail.com>
2025-04-19 11:26:06 +0700

    Update: Vào thẳng tài khoản bỏ qua đăng nhập

--- Program.cs
+++ Program.cs
@@ -1,22 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace QLKS
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
+            // Gọi hàm tạo login và gắn user nếu cần
+            EnsureLoginAndUser();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormDangNhap());
+            Application.Run(new FormChinh(3));
+        }
+
+        static void EnsureLoginAndUser()
+        {
+            try
+            {
+                string connectionString = "Server=(local)\\SQLEXPRESS;Database=master;Integrated Security=True;"; // Kết nối bằng Windows Authentication
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string createLogin = @"
+IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'quanghung')
+BEGIN
+    CREATE LOGIN [quanghung] WITH PASSWORD = '123456', CHECK_EXPIRATION = OFF, CHECK_POLICY = OFF;
+END";
+
+                    string mapUser = @"
+USE [Hotel2025];
+IF EXISTS (SELECT * FROM sys.database_principals WHERE name = 'quanghung')
+BEGIN
+    ALTER USER [quanghung] WITH LOGIN = [quanghung];
+END";
+
+                    using (SqlCommand cmd = new SqlCommand(createLogin, conn))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    using (SqlCommand cmd = new SqlCommand(mapUser, conn))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tạo login hoặc gắn user: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
